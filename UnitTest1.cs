779b79362f930d88e9e59404e5eae25b477688c3
SairaBeg <sairabeg.dev@gmail.com>
2024-01-08 12:12:11 -0500

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Calculator
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        int Sum(int left, int right)
+        {
+            return left + right
+        }
+
+    }
+}
